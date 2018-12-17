5b0458e6828d2e394289bd23ea7a3e3cfba9772f
Tom Stuart <tom@codon.com>
2018-12-17 17:11:44 +0000

    Add test from listing 2.10, “Verse 1 Test”
      ● verse 1
    
        expect(received).toBe(expected) // Object.is equality
    
        Expected: "1 bottle of beer on the wall, 1 bottle of beer.
        Take it down and pass it around, no more bottles of beer on the wall.
        "
        Received: "1 bottles of beer on the wall, 1 bottles of beer.
        Take one down and pass it around, 0 bottles of beer on the wall.
        "
    
        Difference:
    
        - Expected
        + Received
    
        - 1 bottle of beer on the wall, 1 bottle of beer.
        - Take it down and pass it around, no more bottles of beer on the wall.
        + 1 bottles of beer on the wall, 1 bottles of beer.
        + Take one down and pass it around, 0 bottles of beer on the wall.
    
          30 |     'Take it down and pass it around, ' +
          31 |     'no more bottles of beer on the wall.\n';
        > 32 |   expect(new Bottles().verse(1)).toBe(expected);
             |                                  ^
          33 | });
          34 |
    
          at Object.toBe (test/bottles.test.js:32:34)

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -24,4 +24,12 @@ describe('Bottles', () => {
       '1 bottle of beer on the wall.\n';
     expect(new Bottles().verse(2)).toBe(expected);
   });
+
+  test('verse 1', () => {
+    const expected = '1 bottle of beer on the wall, ' +
+      '1 bottle of beer.\n' +
+      'Take it down and pass it around, ' +
+      'no more bottles of beer on the wall.\n';
+    expect(new Bottles().verse(1)).toBe(expected);
+  });
 });
