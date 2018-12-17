2e08c89f064bce2f593a47f675282133915b6719
Tom Stuart <tom@codon.com>
2018-12-17 17:22:26 +0000

    Add test from listing 2.14, “Verses 99 98 Test”
      ● a couple verses
    
        TypeError: (intermediate value).verses is not a function
    
          51 |     'Take one down and pass it around, ' +
          52 |     '97 bottles of beer on the wall.\n';
        > 53 |   expect(new Bottles().verses(99, 98)).toBe(expected);
             |                        ^
          54 | });
          55 |
    
          at Object.verses (test/bottles.test.js:53:24)

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -40,4 +40,17 @@ describe('Bottles', () => {
       '99 bottles of beer on the wall.\n';
     expect(new Bottles().verse(0)).toBe(expected);
   });
+
+  test('a couple verses', () => {
+    const expected = '99 bottles of beer on the wall, ' +
+      '99 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '98 bottles of beer on the wall.\n' +
+      '\n' +
+      '98 bottles of beer on the wall, ' +
+      '98 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '97 bottles of beer on the wall.\n';
+    expect(new Bottles().verses(99, 98)).toBe(expected);
+  });
 });
