fe81dec3b6ee4b16e0ef237c0588cf2f4b8d19c3
Tom Stuart <tom@codon.com>
2019-04-24 23:55:01 +0100

    Copy all other verse tests into the BottleVerse suite

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -385,4 +385,40 @@ describe('BottleVerse', () => {
       '98 bottles of beer on the wall.\n';
     expect(BottleVerse.lyrics(99)).toBe(expected);
   });
+
+  test('another verse', () => {
+    const expected =
+      '3 bottles of beer on the wall, ' +
+      '3 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '2 bottles of beer on the wall.\n';
+    expect(new Bottles().verse(3)).toBe(expected);
+  });
+
+  test('verse 2', () => {
+    const expected =
+      '2 bottles of beer on the wall, ' +
+      '2 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '1 bottle of beer on the wall.\n';
+    expect(new Bottles().verse(2)).toBe(expected);
+  });
+
+  test('verse 1', () => {
+    const expected =
+      '1 bottle of beer on the wall, ' +
+      '1 bottle of beer.\n' +
+      'Take it down and pass it around, ' +
+      'no more bottles of beer on the wall.\n';
+    expect(new Bottles().verse(1)).toBe(expected);
+  });
+
+  test('verse 0', () => {
+    const expected =
+      'No more bottles of beer on the wall, ' +
+      'no more bottles of beer.\n' +
+      'Go to the store and buy some more, ' +
+      '99 bottles of beer on the wall.\n';
+    expect(new Bottles().verse(0)).toBe(expected);
+  });
 });
