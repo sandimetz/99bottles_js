044d7520432a925ffd749eca1296f03588d8fcd4
Tom Stuart <tom@codon.com>
2019-04-24 23:49:48 +0100

    Add test from listing 8.17, “XXX Verse Template Extract Class 13”

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -384,6 +384,6 @@ describe('BottleVerse', () => {
       '99 bottles of beer.\n' +
       'Take one down and pass it around, ' +
       '98 bottles of beer on the wall.\n';
-    expect(new Bottles().verse(99)).toBe(expected);
+    expect(new BottleVerse(99).lyrics()).toBe(expected);
   });
 });
