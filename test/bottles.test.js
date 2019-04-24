92b161cfa7a98c6fd64c59a4fc51c8f38d11d468
Tom Stuart <tom@codon.com>
2019-04-24 20:27:24 +0100

    Add test from listing 8.16, “XXX Verse Template Extract Class 12”

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -377,3 +377,13 @@ Go to the store and buy some more, 99 bottles of beer on the wall.
     expect(new Bottles().song()).toBe(expected);
   });
 });
+
+describe('BottleVerse', () => {
+  test('the first verse', () => {
+    const expected = '99 bottles of beer on the wall, ' +
+      '99 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '98 bottles of beer on the wall.\n';
+    expect(new Bottles().verse(99)).toBe(expected);
+  });
+});
