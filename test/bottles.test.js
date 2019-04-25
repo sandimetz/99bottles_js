a7ec081985e327f4a9fa7ce697f28ff9f02c8724
Tom Stuart <tom@codon.com>
2019-04-25 11:13:31 +0100

    Delete redundant test for a few verses

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -17,25 +17,6 @@ describe('CountdownSong', () => {
     expect(new CountdownSong(VerseFake).verses(99, 97)).toBe(expected);
   });
 
-  test('a few verses', () => {
-    const expected =
-      '2 bottles of beer on the wall, ' +
-      '2 bottles of beer.\n' +
-      'Take one down and pass it around, ' +
-      '1 bottle of beer on the wall.\n' +
-      '\n' +
-      '1 bottle of beer on the wall, ' +
-      '1 bottle of beer.\n' +
-      'Take it down and pass it around, ' +
-      'no more bottles of beer on the wall.\n' +
-      '\n' +
-      'No more bottles of beer on the wall, ' +
-      'no more bottles of beer.\n' +
-      'Go to the store and buy some more, ' +
-      '99 bottles of beer on the wall.\n';
-    expect(new CountdownSong().verses(2, 0)).toBe(expected);
-  });
-
   test('the whole song', () => {
     const expected =
 `99 bottles of beer on the wall, 99 bottles of beer.
