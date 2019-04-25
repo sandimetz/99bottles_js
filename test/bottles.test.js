2257b0b67ef366522c4d86d1ed879361d1f91f4a
Tom Stuart <tom@codon.com>
2019-04-25 11:08:12 +0100

    Remove redundant test for listing 9.7, “XXX Countdownsong 4b”

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -18,19 +18,6 @@ describe('CountDownSong', () => {
     expect(new CountDownSong(FakeVerse).verses(99, 98)).toBe(expected);
   });
 
-  test('a couple verses', () => {
-    const expected = '99 bottles of beer on the wall, ' +
-      '99 bottles of beer.\n' +
-      'Take one down and pass it around, ' +
-      '98 bottles of beer on the wall.\n' +
-      '\n' +
-      '98 bottles of beer on the wall, ' +
-      '98 bottles of beer.\n' +
-      'Take one down and pass it around, ' +
-      '97 bottles of beer on the wall.\n';
-    expect(new CountDownSong().verses(99, 98)).toBe(expected);
-  });
-
   test('a few verses', () => {
     const expected = '2 bottles of beer on the wall, ' +
       '2 bottles of beer.\n' +
