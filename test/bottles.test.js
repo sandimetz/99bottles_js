31903d6a5482445869f660ecf16ac3ef648abe73
Katrina Owen <kytrinyx@github.com>
2020-05-31 08:40:36 -0600

    Add test for verse 99

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -1,4 +1,10 @@
 describe('Bottles', () => {
-  test('', () => {
+  test('the first verse', () => {
+    const expected =
+      '99 bottles of beer on the wall, ' +
+      '99 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '98 bottles of beer on the wall.\n';
+    expect(new Bottles().verse(99)).toBe(expected);
   });
 });
