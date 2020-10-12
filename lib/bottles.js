sandimetz/99bottles_js_milk

Send container message from 0 case, phrase 1

lib/bottles.js
@@ -16,7 +16,8 @@ class Bottles {
       case 0:
         return (
           `${capitalize(this.quantity(number))} ` +
-            'bottles of milk on the wall, ' +
+            `${this.container(number)} ` +
+            'of milk on the wall, ' +
           'no more bottles of milk.\n' +
           'Go to the store and buy some more, ' +
           '99 bottles of milk on the wall.\n'

sandimetz/99bottles_js

lib/bottles.js
@@ -16,7 +16,8 @@ class Bottles {
       case 0:
         return (
           `${capitalize(this.quantity(number))} ` +
-            'bottles of beer on the wall, ' +
+            `${this.container(number)} ` +
+            'of beer on the wall, ' +
           'no more bottles of beer.\n' +
           'Go to the store and buy some more, ' +
           '99 bottles of beer on the wall.\n'