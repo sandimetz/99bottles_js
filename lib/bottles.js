sandimetz/99bottles_js_milk

Pass test for verse 2 with outer conditional

lib/bottles.js
@@ -1,11 +1,20 @@
 class Bottles {
   verse(number) {
-    return (
-      `${number} bottles of milk on the wall, ` +
-      `${number} bottles of milk.\n` +
-      'Take one down and pass it around, ' +
-      `${number-1} bottles of milk on the wall.\n`
-    );
+    if (number === 2) {
+      return (
+        '2 bottles of milk on the wall, ' +
+        '2 bottles of milk.\n' +
+        'Take one down and pass it around, ' +
+        '1 bottle of milk on the wall.\n'
+      );
+    } else {
+      return (
+        `${number} bottles of milk on the wall, ` +
+        `${number} bottles of milk.\n` +
+        'Take one down and pass it around, ' +
+        `${number-1} bottles of milk on the wall.\n`
+      );
+    }
   }
 }
 

sandimetz/99bottles_js

lib/bottles.js
@@ -1,11 +1,20 @@
 class Bottles {
   verse(number) {
-    return (
-      `${number} bottles of beer on the wall, ` +
-      `${number} bottles of beer.\n` +
-      'Take one down and pass it around, ' +
-      `${number-1} bottles of beer on the wall.\n`
-    );
+    if (number === 2) {
+      return (
+        '2 bottles of beer on the wall, ' +
+        '2 bottles of beer.\n' +
+        'Take one down and pass it around, ' +
+        '1 bottle of beer on the wall.\n'
+      );
+    } else {
+      return (
+        `${number} bottles of beer on the wall, ` +
+        `${number} bottles of beer.\n` +
+        'Take one down and pass it around, ' +
+        `${number-1} bottles of beer on the wall.\n`
+      );
+    }
   }
 }
 