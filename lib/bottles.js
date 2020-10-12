sandimetz/99bottles_js_milk

Interpolate number in 2 case

lib/bottles.js
@@ -29,10 +29,10 @@ class Bottles {
         );
       case 2:
         return (
-          '2 bottles of milk on the wall, ' +
-          '2 bottles of milk.\n' +
+          `${number} bottles of milk on the wall, ` +
+          `${number} bottles of milk.\n` +
           'Take one down and pass it around, ' +
-          '1 bottle of milk on the wall.\n'
+          `${number-1} bottle of milk on the wall.\n`
         );
       default:
         return (

sandimetz/99bottles_js

lib/bottles.js
@@ -29,10 +29,10 @@ class Bottles {
         );
       case 2:
         return (
-          '2 bottles of beer on the wall, ' +
-          '2 bottles of beer.\n' +
+          `${number} bottles of beer on the wall, ` +
+          `${number} bottles of beer.\n` +
           'Take one down and pass it around, ' +
-          '1 bottle of beer on the wall.\n'
+          `${number-1} bottle of beer on the wall.\n`
         );
       default:
         return (