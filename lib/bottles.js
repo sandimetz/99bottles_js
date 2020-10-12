sandimetz/99bottles_js_milk

Pull phrase 4 onto single line

lib/bottles.js
@@ -20,8 +20,7 @@ class Bottles {
       capitalize(`${bottleNumber} of milk on the wall, `) +
       `${bottleNumber} of milk.\n` +
       `${bottleNumber.action()}, ` +
-      `${nextBottleNumber} ` +
-        'of milk on the wall.\n'
+      `${nextBottleNumber} ` + 'of milk on the wall.\n'
     );
   }
 }

sandimetz/99bottles_js

lib/bottles.js
@@ -20,8 +20,7 @@ class Bottles {
       capitalize(`${bottleNumber} of beer on the wall, `) +
       `${bottleNumber} of beer.\n` +
       `${bottleNumber.action()}, ` +
-      `${nextBottleNumber} ` +
-        'of beer on the wall.\n'
+      `${nextBottleNumber} ` + 'of beer on the wall.\n'
     );
   }
 }