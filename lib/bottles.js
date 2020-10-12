sandimetz/99bottles_js_milk

Make test for verse 0 pass

lib/bottles.js
@@ -1,6 +1,13 @@
 class Bottles {
   verse(number) {
     switch (number) {
+      case 0:
+        return (
+          'No more bottles of milk on the wall, ' +
+          'no more bottles of milk.\n' +
+          'Go to the store and buy some more, ' +
+          '99 bottles of milk on the wall.\n'
+        );
       case 1:
         return (
           '1 bottle of milk on the wall, ' +

sandimetz/99bottles_js

lib/bottles.js
@@ -1,6 +1,13 @@
 class Bottles {
   verse(number) {
     switch (number) {
+      case 0:
+        return (
+          'No more bottles of beer on the wall, ' +
+          'no more bottles of beer.\n' +
+          'Go to the store and buy some more, ' +
+          '99 bottles of beer on the wall.\n'
+        );
       case 1:
         return (
           '1 bottle of beer on the wall, ' +