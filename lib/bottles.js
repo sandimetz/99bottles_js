sandimetz/99bottles_js_milk

Try common template

lib/bottles.js
@@ -12,6 +12,19 @@ class Bottles {
   }
 
   verse(number) {
+    return (
+      `${capitalize(this.quantity(number))} ` +
+        `${this.container(number)} ` +
+        'of milk on the wall, ' +
+      `${this.quantity(number)} ` +
+        `${this.container(number)} ` +
+        'of milk.\n' +
+      `${this.action(number)}, ` +
+      `${this.quantity(this.successor(number))} ` +
+        `${this.container(this.successor(number))} ` +
+        'of milk on the wall.\n'
+    );
+
     switch (number) {
       case 0:
         return (

sandimetz/99bottles_js

lib/bottles.js
@@ -12,6 +12,19 @@ class Bottles {
   }
 
   verse(number) {
+    return (
+      `${capitalize(this.quantity(number))} ` +
+        `${this.container(number)} ` +
+        'of beer on the wall, ' +
+      `${this.quantity(number)} ` +
+        `${this.container(number)} ` +
+        'of beer.\n' +
+      `${this.action(number)}, ` +
+      `${this.quantity(this.successor(number))} ` +
+        `${this.container(this.successor(number))} ` +
+        'of beer on the wall.\n'
+    );
+
     switch (number) {
       case 0:
         return (