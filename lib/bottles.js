sandimetz/99bottles_js_milk

Undo container method change

lib/bottles.js
@@ -32,25 +32,15 @@ class Bottles {
           `${number} bottles of milk on the wall, ` +
           `${number} bottles of milk.\n` +
           'Take one down and pass it around, ' +
-          `${number-1} ${this.container(number-1)} ` +
-            'of milk on the wall.\n'
+          `${number-1} bottle of milk on the wall.\n`
         );
       default:
         return (
           `${number} bottles of milk on the wall, ` +
           `${number} bottles of milk.\n` +
           'Take one down and pass it around, ' +
-          `${number-1} ${this.container(number-1)} ` +
-            'of milk on the wall.\n'
+          `${number-1} bottles of milk on the wall.\n`
         );
-    }
-  }
-
-  container(number) {
-    if (number === 1) {
-      return 'bottle';
-    } else {
-      return 'bottles';
     }
   }
 }

sandimetz/99bottles_js

lib/bottles.js
@@ -32,25 +32,15 @@ class Bottles {
           `${number} bottles of beer on the wall, ` +
           `${number} bottles of beer.\n` +
           'Take one down and pass it around, ' +
-          `${number-1} ${this.container(number-1)} ` +
-            'of beer on the wall.\n'
+          `${number-1} bottle of beer on the wall.\n`
         );
       default:
         return (
           `${number} bottles of beer on the wall, ` +
           `${number} bottles of beer.\n` +
           'Take one down and pass it around, ' +
-          `${number-1} ${this.container(number-1)} ` +
-            'of beer on the wall.\n'
+          `${number-1} bottles of beer on the wall.\n`
         );
-    }
-  }
-
-  container(number) {
-    if (number === 1) {
-      return 'bottle';
-    } else {
-      return 'bottles';
     }
   }
 }