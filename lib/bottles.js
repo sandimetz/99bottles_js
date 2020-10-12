sandimetz/99bottles_js_milk

Interpolate number at start of one case

lib/bottles.js
@@ -22,7 +22,7 @@ class Bottles {
         );
       case 1:
         return (
-          '1 bottle of milk on the wall, ' +
+          `${number} bottle of milk on the wall, ` +
           '1 bottle of milk.\n' +
           'Take it down and pass it around, ' +
           'no more bottles of milk on the wall.\n'

sandimetz/99bottles_js

lib/bottles.js
@@ -22,7 +22,7 @@ class Bottles {
         );
       case 1:
         return (
-          '1 bottle of beer on the wall, ' +
+          `${number} bottle of beer on the wall, ` +
           '1 bottle of beer.\n' +
           'Take it down and pass it around, ' +
           'no more bottles of beer on the wall.\n'