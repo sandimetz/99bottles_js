sandimetz/99bottles_js_milk

Make phrase 2 of case 1 and default identical

lib/bottles.js
@@ -24,7 +24,7 @@ class Bottles {
         return (
           `${number} ${this.container(number)} ` +
             'of milk on the wall, ' +
-          '1 bottle of milk.\n' +
+          `${number} ${this.container(number)} of milk.\n` +
           'Take it down and pass it around, ' +
           'no more bottles of milk on the wall.\n'
         );
@@ -32,7 +32,7 @@ class Bottles {
         return (
           `${number} ${this.container(number)} ` +
             'of milk on the wall, ' +
-          `${number} bottles of milk.\n` +
+          `${number} ${this.container(number)} of milk.\n` +
           'Take one down and pass it around, ' +
           `${number-1} ${this.container(number-1)} ` +
             'of milk on the wall.\n'

sandimetz/99bottles_js

lib/bottles.js
@@ -24,7 +24,7 @@ class Bottles {
         return (
           `${number} ${this.container(number)} ` +
             'of beer on the wall, ' +
-          '1 bottle of beer.\n' +
+          `${number} ${this.container(number)} of beer.\n` +
           'Take it down and pass it around, ' +
           'no more bottles of beer on the wall.\n'
         );
@@ -32,7 +32,7 @@ class Bottles {
         return (
           `${number} ${this.container(number)} ` +
             'of beer on the wall, ' +
-          `${number} bottles of beer.\n` +
+          `${number} ${this.container(number)} of beer.\n` +
           'Take one down and pass it around, ' +
           `${number-1} ${this.container(number-1)} ` +
             'of beer on the wall.\n'