sandimetz/99bottles_js_milk

Send quantity from phrase 1 in case 1

lib/bottles.js
@@ -1,4 +1,4 @@
-import { downTo } from './helpers';
+import { capitalize, downTo } from './helpers';
 
 class Bottles {
   song() {
@@ -15,7 +15,8 @@ class Bottles {
     switch (number) {
       case 0:
         return (
-          'No more bottles of milk on the wall, ' +
+          `${capitalize(this.quantity(number))} ` +
+            'bottles of milk on the wall, ' +
           'no more bottles of milk.\n' +
           'Go to the store and buy some more, ' +
           '99 bottles of milk on the wall.\n'

sandimetz/99bottles_js

lib/bottles.js
@@ -1,4 +1,4 @@
-import { downTo } from './helpers';
+import { capitalize, downTo } from './helpers';
 
 class Bottles {
   song() {
@@ -15,7 +15,8 @@ class Bottles {
     switch (number) {
       case 0:
         return (
-          'No more bottles of beer on the wall, ' +
+          `${capitalize(this.quantity(number))} ` +
+            'bottles of beer on the wall, ' +
           'no more bottles of beer.\n' +
           'Go to the store and buy some more, ' +
           '99 bottles of beer on the wall.\n'