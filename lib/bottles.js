sandimetz/99bottles_js_milk

Add shameless green example

lib/bottles.js
@@ -1,99 +1,47 @@
-import { capitalize, downTo } from './helpers';
+import { downTo } from './helpers';
 
 class Bottles {
   song() {
     return this.verses(99, 0);
   }
 
-  verses(bottlesAtStart, bottlesAtEnd) {
-    return downTo(bottlesAtStart, bottlesAtEnd)
-      .map(bottles => this.verse(bottles))
+  verses(upper, lower) {
+    return downTo(upper, lower)
+      .map(i => this.verse(i))
       .join('\n');
   }
 
-  verse(bottles) {
-    return new Round(bottles).toString();
-  }
-}
-
-class Round {
-  constructor(bottles) {
-    this.bottles = bottles;
-  }
-
-  toString() {
-    return this.challenge() + this.response();
-  }
-
-  challenge() {
-    return (
-      capitalize(this.bottlesOfBeer()) + ' ' +
-      this.onWall() + ', ' +
-      this.bottlesOfBeer() + '.\n'
-    );
-  }
-
-  response() {
-    return (
-      this.goToTheStoreOrTakeOneDown() + ', ' +
-      this.bottlesOfBeer() + ' ' +
-      this.onWall() + '.\n'
-    );
-  }
-
-  bottlesOfBeer() {
-    return (
-      this.anglicizedBottleCount() + ' ' +
-      this.pluralizedBottleForm() + ' of ' +
-      this.milk()
-    );
-  }
-
-  milk() {
-    return 'milk';
-  }
-
-  onWall() {
-    return 'on the wall';
-  }
-
-  pluralizedBottleForm() {
-    return this.isLastBeer() ? 'bottle' : 'bottles';
-  }
-
-  anglicizedBottleCount() {
-    return this.isAllOut() ? 'no more' : this.bottles.toString();
-  }
-
-  goToTheStoreOrTakeOneDown() {
-    if (this.isAllOut()) {
-      this.bottles = 99;
-      return this.buyNewBeer();
-    } else {
-      const lyrics = this.drinkBeer();
-      this.bottles--;
-      return lyrics;
+  verse(number) {
+    switch (number) {
+      case 0:
+        return (
+          'No more bottles of milk on the wall, ' +
+          'no more bottles of milk.\n' +
+          'Go to the store and buy some more, ' +
+          '99 bottles of milk on the wall.\n'
+        );
+      case 1:
+        return (
+          '1 bottle of milk on the wall, ' +
+          '1 bottle of milk.\n' +
+          'Take it down and pass it around, ' +
+          'no more bottles of milk on the wall.\n'
+        );
+      case 2:
+        return (
+          '2 bottles of milk on the wall, ' +
+          '2 bottles of milk.\n' +
+          'Take one down and pass it around, ' +
+          '1 bottle of milk on the wall.\n'
+        );
+      default:
+        return (
+          `${number} bottles of milk on the wall, ` +
+          `${number} bottles of milk.\n` +
+          'Take one down and pass it around, ' +
+          `${number-1} bottles of milk on the wall.\n`
+        );
     }
-  }
-
-  buyNewBeer() {
-    return 'Go to the store and buy some more';
-  }
-
-  drinkBeer() {
-    return `Take ${this.itOrOne()} down and pass it around`;
-  }
-
-  itOrOne() {
-    return this.isLastBeer() ? 'it' : 'one';
-  }
-
-  isAllOut() {
-    return this.bottles === 0;
-  }
-
-  isLastBeer() {
-    return this.bottles === 1;
   }
 }
 

sandimetz/99bottles_js

lib/bottles.js
@@ -1,99 +1,47 @@
-import { capitalize, downTo } from './helpers';
+import { downTo } from './helpers';
 
 class Bottles {
   song() {
     return this.verses(99, 0);
   }
 
-  verses(bottlesAtStart, bottlesAtEnd) {
-    return downTo(bottlesAtStart, bottlesAtEnd)
-      .map(bottles => this.verse(bottles))
+  verses(upper, lower) {
+    return downTo(upper, lower)
+      .map(i => this.verse(i))
       .join('\n');
   }
 
-  verse(bottles) {
-    return new Round(bottles).toString();
-  }
-}
-
-class Round {
-  constructor(bottles) {
-    this.bottles = bottles;
-  }
-
-  toString() {
-    return this.challenge() + this.response();
-  }
-
-  challenge() {
-    return (
-      capitalize(this.bottlesOfBeer()) + ' ' +
-      this.onWall() + ', ' +
-      this.bottlesOfBeer() + '.\n'
-    );
-  }
-
-  response() {
-    return (
-      this.goToTheStoreOrTakeOneDown() + ', ' +
-      this.bottlesOfBeer() + ' ' +
-      this.onWall() + '.\n'
-    );
-  }
-
-  bottlesOfBeer() {
-    return (
-      this.anglicizedBottleCount() + ' ' +
-      this.pluralizedBottleForm() + ' of ' +
-      this.beer()
-    );
-  }
-
-  beer() {
-    return 'beer';
-  }
-
-  onWall() {
-    return 'on the wall';
-  }
-
-  pluralizedBottleForm() {
-    return this.isLastBeer() ? 'bottle' : 'bottles';
-  }
-
-  anglicizedBottleCount() {
-    return this.isAllOut() ? 'no more' : this.bottles.toString();
-  }
-
-  goToTheStoreOrTakeOneDown() {
-    if (this.isAllOut()) {
-      this.bottles = 99;
-      return this.buyNewBeer();
-    } else {
-      const lyrics = this.drinkBeer();
-      this.bottles--;
-      return lyrics;
+  verse(number) {
+    switch (number) {
+      case 0:
+        return (
+          'No more bottles of beer on the wall, ' +
+          'no more bottles of beer.\n' +
+          'Go to the store and buy some more, ' +
+          '99 bottles of beer on the wall.\n'
+        );
+      case 1:
+        return (
+          '1 bottle of beer on the wall, ' +
+          '1 bottle of beer.\n' +
+          'Take it down and pass it around, ' +
+          'no more bottles of beer on the wall.\n'
+        );
+      case 2:
+        return (
+          '2 bottles of beer on the wall, ' +
+          '2 bottles of beer.\n' +
+          'Take one down and pass it around, ' +
+          '1 bottle of beer on the wall.\n'
+        );
+      default:
+        return (
+          `${number} bottles of beer on the wall, ` +
+          `${number} bottles of beer.\n` +
+          'Take one down and pass it around, ' +
+          `${number-1} bottles of beer on the wall.\n`
+        );
     }
-  }
-
-  buyNewBeer() {
-    return 'Go to the store and buy some more';
-  }
-
-  drinkBeer() {
-    return `Take ${this.itOrOne()} down and pass it around`;
-  }
-
-  itOrOne() {
-    return this.isLastBeer() ? 'it' : 'one';
-  }
-
-  isAllOut() {
-    return this.bottles === 0;
-  }
-
-  isLastBeer() {
-    return this.bottles === 1;
   }
 }
 