sandimetz/99bottles_js_milk

Add test for verse 3

test/bottles.test.js
@@ -9,4 +9,13 @@ describe('Bottles', () => {
       '98 bottles of milk on the wall.\n';
     expect(new Bottles().verse(99)).toBe(expected);
   });
+
+  test('another verse', () => {
+    const expected =
+      '3 bottles of milk on the wall, ' +
+      '3 bottles of milk.\n' +
+      'Take one down and pass it around, ' +
+      '2 bottles of milk on the wall.\n';
+    expect(new Bottles().verse(3)).toBe(expected);
+  });
 });

sandimetz/99bottles_js

test/bottles.test.js
@@ -9,4 +9,13 @@ describe('Bottles', () => {
       '98 bottles of beer on the wall.\n';
     expect(new Bottles().verse(99)).toBe(expected);
   });
+
+  test('another verse', () => {
+    const expected =
+      '3 bottles of beer on the wall, ' +
+      '3 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '2 bottles of beer on the wall.\n';
+    expect(new Bottles().verse(3)).toBe(expected);
+  });
 });
baa0b4201a1d83341cd8545d717a18e0b31830b3
Tom Stuart <tom@codon.com>
2018-12-17 10:52:37 +0000

    Add test for verse 3

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -9,4 +9,13 @@ describe('Bottles', () => {
       '98 bottles of beer on the wall.\n';
     expect(new Bottles().verse(99)).toBe(expected);
   });
+
+  test('another verse', () => {
+    const expected =
+      '3 bottles of beer on the wall, ' +
+      '3 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '2 bottles of beer on the wall.\n';
+    expect(new Bottles().verse(3)).toBe(expected);
+  });
 });
