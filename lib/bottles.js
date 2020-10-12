sandimetz/99bottles_js_milk

Send container from default case

lib/bottles.js
@@ -39,7 +39,8 @@ class Bottles {
           `${number} bottles of milk on the wall, ` +
           `${number} bottles of milk.\n` +
           'Take one down and pass it around, ' +
-          `${number-1} bottles of milk on the wall.\n`
+          `${number-1} ${this.container()} ` +
+            'of milk on the wall.\n'
         );
     }
   }

sandimetz/99bottles_js

lib/bottles.js
@@ -39,7 +39,8 @@ class Bottles {
           `${number} bottles of beer on the wall, ` +
           `${number} bottles of beer.\n` +
           'Take one down and pass it around, ' +
-          `${number-1} bottles of beer on the wall.\n`
+          `${number-1} ${this.container()} ` +
+            'of beer on the wall.\n'
         );
     }
   }