sandimetz/99bottles_js_milk

Inline nextBottleNumber temporary variable

lib/bottles.js
@@ -19,7 +19,7 @@ class Bottles {
       capitalize(`${bottleNumber} of milk on the wall, `) +
       `${bottleNumber} of milk.\n` +
       `${bottleNumber.action()}, ` +
-      `${nextBottleNumber} of milk on the wall.\n`
+      `${bottleNumber.successor()} of milk on the wall.\n`
     );
   }
 }

sandimetz/99bottles_js

lib/bottles.js
@@ -19,7 +19,7 @@ class Bottles {
       capitalize(`${bottleNumber} of beer on the wall, `) +
       `${bottleNumber} of beer.\n` +
       `${bottleNumber.action()}, ` +
-      `${nextBottleNumber} of beer on the wall.\n`
+      `${bottleNumber.successor()} of beer on the wall.\n`
     );
   }
 }