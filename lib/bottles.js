sandimetz/99bottles_js_milk

Separate conditional from lyrics

lib/bottles.js
@@ -1,20 +1,19 @@
 class Bottles {
   verse(number) {
+    let n;
+
     if (number === 99) {
-      return (
-        '99 bottles of milk on the wall, ' +
-        '99 bottles of milk.\n' +
-        'Take one down and pass it around, ' +
-        '98 bottles of milk on the wall.\n'
-      );
+      n = 99;
     } else {
-      return (
-        '3 bottles of milk on the wall, ' +
-        '3 bottles of milk.\n' +
-        'Take one down and pass it around, ' +
-        '2 bottles of milk on the wall.\n'
-      );
+      n = 3;
     }
+
+    return (
+      `${n} bottles of milk on the wall, ` +
+      `${n} bottles of milk.\n` +
+      'Take one down and pass it around, ' +
+      `${n - 1} bottles of milk on the wall.\n`
+    );
   }
 }
 

sandimetz/99bottles_js

lib/bottles.js
@@ -1,20 +1,19 @@
 class Bottles {
   verse(number) {
+    let n;
+
     if (number === 99) {
-      return (
-        '99 bottles of beer on the wall, ' +
-        '99 bottles of beer.\n' +
-        'Take one down and pass it around, ' +
-        '98 bottles of beer on the wall.\n'
-      );
+      n = 99;
     } else {
-      return (
-        '3 bottles of beer on the wall, ' +
-        '3 bottles of beer.\n' +
-        'Take one down and pass it around, ' +
-        '2 bottles of beer on the wall.\n'
-      );
+      n = 3;
     }
+
+    return (
+      `${n} bottles of beer on the wall, ` +
+      `${n} bottles of beer.\n` +
+      'Take one down and pass it around, ' +
+      `${n - 1} bottles of beer on the wall.\n`
+    );
   }
 }
 