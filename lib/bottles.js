sandimetz/99bottles_js_milk

Consolidate template in phrase 1

lib/bottles.js
@@ -17,7 +17,7 @@ class Bottles {
       new BottleNumber(bottleNumber.successor());
 
     return (
-      capitalize(`${bottleNumber} `) + 'of milk on the wall, ' +
+      capitalize(`${bottleNumber} of milk on the wall, `) +
       `${bottleNumber} ` +
         'of milk.\n' +
       `${bottleNumber.action()}, ` +

sandimetz/99bottles_js

lib/bottles.js
@@ -17,7 +17,7 @@ class Bottles {
       new BottleNumber(bottleNumber.successor());
 
     return (
-      capitalize(`${bottleNumber} `) + 'of beer on the wall, ' +
+      capitalize(`${bottleNumber} of beer on the wall, `) +
       `${bottleNumber} ` +
         'of beer.\n' +
       `${bottleNumber.action()}, ` +