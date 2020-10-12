sandimetz/99bottles_js_milk

Make test for verse 99 pass

lib/bottles.js
@@ -1,5 +1,11 @@
 class Bottles {
   verse() {
+    return (
+      '99 bottles of milk on the wall, ' +
+      '99 bottles of milk.\n' +
+      'Take one down and pass it around, ' +
+      '98 bottles of milk on the wall.\n'
+    );
   }
 }
 

sandimetz/99bottles_js

lib/bottles.js
@@ -1,5 +1,11 @@
 class Bottles {
   verse() {
+    return (
+      '99 bottles of beer on the wall, ' +
+      '99 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '98 bottles of beer on the wall.\n'
+    );
   }
 }
 