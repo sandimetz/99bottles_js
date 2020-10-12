sandimetz/99bottles_js_milk

Pass argument to quantity message in phrase 4 of case 1

lib/bottles.js
@@ -26,7 +26,8 @@ class Bottles {
             'of milk on the wall, ' +
           `${number} ${this.container(number)} of milk.\n` +
           `Take ${this.pronoun(number)} down and pass it around, ` +
-          `${this.quantity()} bottles of milk on the wall.\n`
+          `${this.quantity(number-1)} bottles ` +
+            'of milk on the wall.\n'
         );
       default:
         return (

sandimetz/99bottles_js

lib/bottles.js
@@ -26,7 +26,8 @@ class Bottles {
             'of beer on the wall, ' +
           `${number} ${this.container(number)} of beer.\n` +
           `Take ${this.pronoun(number)} down and pass it around, ` +
-          `${this.quantity()} bottles of beer on the wall.\n`
+          `${this.quantity(number-1)} bottles ` +
+            'of beer on the wall.\n'
         );
       default:
         return (