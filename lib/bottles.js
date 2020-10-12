sandimetz/99bottles_js_milk

Pass 3 and 99 with conditional

lib/bottles.js
@@ -1,11 +1,20 @@
 class Bottles {
-  verse() {
-    return (
-      '99 bottles of milk on the wall, ' +
-      '99 bottles of milk.\n' +
-      'Take one down and pass it around, ' +
-      '98 bottles of milk on the wall.\n'
-    );
+  verse(number) {
+    if (number === 99) {
+      return (
+        '99 bottles of milk on the wall, ' +
+        '99 bottles of milk.\n' +
+        'Take one down and pass it around, ' +
+        '98 bottles of milk on the wall.\n'
+      );
+    } else {
+      return (
+        '3 bottles of milk on the wall, ' +
+        '3 bottles of milk.\n' +
+        'Take one down and pass it around, ' +
+        '2 bottles of milk on the wall.\n'
+      );
+    }
   }
 }
 

sandimetz/99bottles_js

lib/bottles.js
@@ -1,11 +1,20 @@
 class Bottles {
-  verse() {
-    return (
-      '99 bottles of beer on the wall, ' +
-      '99 bottles of beer.\n' +
-      'Take one down and pass it around, ' +
-      '98 bottles of beer on the wall.\n'
-    );
+  verse(number) {
+    if (number === 99) {
+      return (
+        '99 bottles of beer on the wall, ' +
+        '99 bottles of beer.\n' +
+        'Take one down and pass it around, ' +
+        '98 bottles of beer on the wall.\n'
+      );
+    } else {
+      return (
+        '3 bottles of beer on the wall, ' +
+        '3 bottles of beer.\n' +
+        'Take one down and pass it around, ' +
+        '2 bottles of beer on the wall.\n'
+      );
+    }
   }
 }
 