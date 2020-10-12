sandimetz/99bottles_js_milk

Add six-pack to song test

test/bottles.test.js
@@ -358,9 +358,9 @@ Take one down and pass it around, 8 bottles of milk on the wall.
 Take one down and pass it around, 7 bottles of milk on the wall.
 
 7 bottles of milk on the wall, 7 bottles of milk.
-Take one down and pass it around, 6 bottles of milk on the wall.
+Take one down and pass it around, 1 six-pack of milk on the wall.
 
-6 bottles of milk on the wall, 6 bottles of milk.
+1 six-pack of milk on the wall, 1 six-pack of milk.
 Take one down and pass it around, 5 bottles of milk on the wall.
 
 5 bottles of milk on the wall, 5 bottles of milk.

sandimetz/99bottles_js

test/bottles.test.js
@@ -358,9 +358,9 @@ Take one down and pass it around, 8 bottles of beer on the wall.
 Take one down and pass it around, 7 bottles of beer on the wall.
 
 7 bottles of beer on the wall, 7 bottles of beer.
-Take one down and pass it around, 6 bottles of beer on the wall.
+Take one down and pass it around, 1 six-pack of beer on the wall.
 
-6 bottles of beer on the wall, 6 bottles of beer.
+1 six-pack of beer on the wall, 1 six-pack of beer.
 Take one down and pass it around, 5 bottles of beer on the wall.
 
 5 bottles of beer on the wall, 5 bottles of beer.