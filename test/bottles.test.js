sandimetz/99bottles_js_milk

Add test for verses 2, 1, and 0

test/bottles.test.js
@@ -59,4 +59,23 @@ describe('Bottles', () => {
       '97 bottles of milk on the wall.\n';
     expect(new Bottles().verses(99, 98)).toBe(expected);
   });
+
+  test('a few verses', () => {
+    const expected =
+      '2 bottles of milk on the wall, ' +
+      '2 bottles of milk.\n' +
+      'Take one down and pass it around, ' +
+      '1 bottle of milk on the wall.\n' +
+      '\n' +
+      '1 bottle of milk on the wall, ' +
+      '1 bottle of milk.\n' +
+      'Take it down and pass it around, ' +
+      'no more bottles of milk on the wall.\n' +
+      '\n' +
+      'No more bottles of milk on the wall, ' +
+      'no more bottles of milk.\n' +
+      'Go to the store and buy some more, ' +
+      '99 bottles of milk on the wall.\n';
+    expect(new Bottles().verses(2, 0)).toBe(expected);
+  });
 });

sandimetz/99bottles_js

test/bottles.test.js
@@ -59,4 +59,23 @@ describe('Bottles', () => {
       '97 bottles of beer on the wall.\n';
     expect(new Bottles().verses(99, 98)).toBe(expected);
   });
+
+  test('a few verses', () => {
+    const expected =
+      '2 bottles of beer on the wall, ' +
+      '2 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '1 bottle of beer on the wall.\n' +
+      '\n' +
+      '1 bottle of beer on the wall, ' +
+      '1 bottle of beer.\n' +
+      'Take it down and pass it around, ' +
+      'no more bottles of beer on the wall.\n' +
+      '\n' +
+      'No more bottles of beer on the wall, ' +
+      'no more bottles of beer.\n' +
+      'Go to the store and buy some more, ' +
+      '99 bottles of beer on the wall.\n';
+    expect(new Bottles().verses(2, 0)).toBe(expected);
+  });
 });