sandimetz/99bottles_js_milk

Pass verse 1 test

lib/bottles.js
@@ -1,19 +1,27 @@
 class Bottles {
   verse(number) {
-    if (number === 2) {
-      return (
-        '2 bottles of milk on the wall, ' +
-        '2 bottles of milk.\n' +
-        'Take one down and pass it around, ' +
-        '1 bottle of milk on the wall.\n'
-      );
-    } else {
-      return (
-        `${number} bottles of milk on the wall, ` +
-        `${number} bottles of milk.\n` +
-        'Take one down and pass it around, ' +
-        `${number-1} bottles of milk on the wall.\n`
-      );
+    switch (number) {
+      case 1:
+        return (
+          '1 bottle of milk on the wall, ' +
+          '1 bottle of milk.\n' +
+          'Take it down and pass it around, ' +
+          'no more bottles of milk on the wall.\n'
+        );
+      case 2:
+        return (
+          '2 bottles of milk on the wall, ' +
+          '2 bottles of milk.\n' +
+          'Take one down and pass it around, ' +
+          '1 bottle of milk on the wall.\n'
+        );
+      default:
+        return (
+          `${number} bottles of milk on the wall, ` +
+          `${number} bottles of milk.\n` +
+          'Take one down and pass it around, ' +
+          `${number-1} bottles of milk on the wall.\n`
+        );
     }
   }
 }

sandimetz/99bottles_js

lib/bottles.js
@@ -1,19 +1,27 @@
 class Bottles {
   verse(number) {
-    if (number === 2) {
-      return (
-        '2 bottles of beer on the wall, ' +
-        '2 bottles of beer.\n' +
-        'Take one down and pass it around, ' +
-        '1 bottle of beer on the wall.\n'
-      );
-    } else {
-      return (
-        `${number} bottles of beer on the wall, ` +
-        `${number} bottles of beer.\n` +
-        'Take one down and pass it around, ' +
-        `${number-1} bottles of beer on the wall.\n`
-      );
+    switch (number) {
+      case 1:
+        return (
+          '1 bottle of beer on the wall, ' +
+          '1 bottle of beer.\n' +
+          'Take it down and pass it around, ' +
+          'no more bottles of beer on the wall.\n'
+        );
+      case 2:
+        return (
+          '2 bottles of beer on the wall, ' +
+          '2 bottles of beer.\n' +
+          'Take one down and pass it around, ' +
+          '1 bottle of beer on the wall.\n'
+        );
+      default:
+        return (
+          `${number} bottles of beer on the wall, ` +
+          `${number} bottles of beer.\n` +
+          'Take one down and pass it around, ' +
+          `${number-1} bottles of beer on the wall.\n`
+        );
     }
   }
 }