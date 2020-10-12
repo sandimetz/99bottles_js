sandimetz/99bottles_js_milk

Send quantity with 99 from 0 case

lib/bottles.js
@@ -22,7 +22,8 @@ class Bottles {
             `${this.container(number)} ` +
             'of milk.\n' +
           `${this.action(number)}, ` +
-          '99 bottles of milk on the wall.\n'
+          `${this.quantity(99)} bottles ` +
+            'of milk on the wall.\n'
         );
       default:
         return (

sandimetz/99bottles_js

lib/bottles.js
@@ -22,7 +22,8 @@ class Bottles {
             `${this.container(number)} ` +
             'of beer.\n' +
           `${this.action(number)}, ` +
-          '99 bottles of beer on the wall.\n'
+          `${this.quantity(99)} bottles ` +
+            'of beer on the wall.\n'
         );
       default:
         return (