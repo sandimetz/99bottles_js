sandimetz/99bottles_js_milk

Make case 0, phrase 2 identical to default case

lib/bottles.js
@@ -18,7 +18,9 @@ class Bottles {
           `${capitalize(this.quantity(number))} ` +
             `${this.container(number)} ` +
             'of milk on the wall, ' +
-          'no more bottles of milk.\n' +
+          `${this.quantity(number)} ` +
+            `${this.container(number)} ` +
+            'of milk.\n' +
           'Go to the store and buy some more, ' +
           '99 bottles of milk on the wall.\n'
         );

sandimetz/99bottles_js

lib/bottles.js
@@ -18,7 +18,9 @@ class Bottles {
           `${capitalize(this.quantity(number))} ` +
             `${this.container(number)} ` +
             'of beer on the wall, ' +
-          'no more bottles of beer.\n' +
+          `${this.quantity(number)} ` +
+            `${this.container(number)} ` +
+            'of beer.\n' +
           'Go to the store and buy some more, ' +
           '99 bottles of beer on the wall.\n'
         );