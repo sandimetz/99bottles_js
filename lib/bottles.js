sandimetz/99bottles_js_milk

Make verses test pass with literal

lib/bottles.js
@@ -1,4 +1,18 @@
 class Bottles {
+  verses() {
+    return (
+      '99 bottles of milk on the wall, ' +
+      '99 bottles of milk.\n' +
+      'Take one down and pass it around, ' +
+      '98 bottles of milk on the wall.\n' +
+      '\n' +
+      '98 bottles of milk on the wall, ' +
+      '98 bottles of milk.\n' +
+      'Take one down and pass it around, ' +
+      '97 bottles of milk on the wall.\n'
+    );
+  }
+
   verse(number) {
     switch (number) {
       case 0:

sandimetz/99bottles_js

lib/bottles.js
@@ -1,4 +1,18 @@
 class Bottles {
+  verses() {
+    return (
+      '99 bottles of beer on the wall, ' +
+      '99 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '98 bottles of beer on the wall.\n' +
+      '\n' +
+      '98 bottles of beer on the wall, ' +
+      '98 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '97 bottles of beer on the wall.\n'
+    );
+  }
+
   verse(number) {
     switch (number) {
       case 0: