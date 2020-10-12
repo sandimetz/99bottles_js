sandimetz/99bottles_js_milk

Consolidate string template for phrase 2

lib/bottles.js
@@ -18,7 +18,7 @@ class Bottles {
 
     return (
       capitalize(`${bottleNumber} of milk on the wall, `) +
-      `${bottleNumber} ` + 'of milk.\n' +
+      `${bottleNumber} of milk.\n` +
       `${bottleNumber.action()}, ` +
       `${nextBottleNumber} ` +
         'of milk on the wall.\n'

sandimetz/99bottles_js

lib/bottles.js
@@ -18,7 +18,7 @@ class Bottles {
 
     return (
       capitalize(`${bottleNumber} of beer on the wall, `) +
-      `${bottleNumber} ` + 'of beer.\n' +
+      `${bottleNumber} of beer.\n` +
       `${bottleNumber.action()}, ` +
       `${nextBottleNumber} ` +
         'of beer on the wall.\n'