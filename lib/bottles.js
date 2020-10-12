sandimetz/99bottles_js_milk

Revert to old verse

lib/bottles.js
@@ -12,15 +12,15 @@ class Bottles {
   }
 
   verse(number) {
-    return new BottleVerse(number).verse(number);
-    // const bottleNumber = BottleNumber.for(number);
+    new BottleVerse(number).verse(number);
+    const bottleNumber = BottleNumber.for(number);
 
-    // return (
-    //   capitalize(`${bottleNumber} of milk on the wall, `) +
-    //   `${bottleNumber} of milk.\n` +
-    //   `${bottleNumber.action()}, ` +
-    //   `${bottleNumber.successor()} of milk on the wall.\n`
-    // );
+    return (
+      capitalize(`${bottleNumber} of milk on the wall, `) +
+      `${bottleNumber} of milk.\n` +
+      `${bottleNumber.action()}, ` +
+      `${bottleNumber.successor()} of milk on the wall.\n`
+    );
   }
 }
 

sandimetz/99bottles_js

lib/bottles.js
@@ -12,15 +12,15 @@ class Bottles {
   }
 
   verse(number) {
-    return new BottleVerse(number).verse(number);
-    // const bottleNumber = BottleNumber.for(number);
+    new BottleVerse(number).verse(number);
+    const bottleNumber = BottleNumber.for(number);
 
-    // return (
-    //   capitalize(`${bottleNumber} of beer on the wall, `) +
-    //   `${bottleNumber} of beer.\n` +
-    //   `${bottleNumber.action()}, ` +
-    //   `${bottleNumber.successor()} of beer on the wall.\n`
-    // );
+    return (
+      capitalize(`${bottleNumber} of beer on the wall, `) +
+      `${bottleNumber} of beer.\n` +
+      `${bottleNumber.action()}, ` +
+      `${bottleNumber.successor()} of beer on the wall.\n`
+    );
   }
 }
 