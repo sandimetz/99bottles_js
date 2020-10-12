sandimetz/99bottles_js_milk

Revert badly broken song code

lib/bottles.js
@@ -6,24 +6,9 @@ class Bottles {
   }
 
   verses(starting, ending) {
-    if (starting === 99 && ending === 98) {
-      return (
-        '99 bottles of milk on the wall, ' +
-        '99 bottles of milk.\n' +
-        'Take one down and pass it around, ' +
-        '98 bottles of milk on the wall.\n' +
-        '\n' +
-        '98 bottles of milk on the wall, ' +
-        '98 bottles of milk.\n' +
-        'Take one down and pass it around, ' +
-        '97 bottles of milk on the wall.\n'
-      );
-    } else if (starting === 2) {
-      return this.verse(2) + '\n' + this.verse(1) +
-      '\n' + this.verse(0);
-    } else {
-      return 'ok';
-    }
+    return downTo(starting, ending)
+      .map(i => this.verse(i))
+      .join('\n');
   }
 
   verse(number) {

sandimetz/99bottles_js

lib/bottles.js
@@ -6,20 +6,9 @@ class Bottles {
   }
 
   verses(upper, lower) {
-    if (upper === 99 && lower === 98) {
-      return (
-        this.verse(99) + '\n' +
-        this.verse(98)
-      );
-    } else if (upper === 2) {
-      return (
-        this.verse(2) + '\n' +
-        this.verse(1) + '\n' +
-        this.verse(0)
-      );
-    } else {
-      return 'ok';
-    }
+    return downTo(upper, lower)
+      .map(i => this.verse(i))
+      .join('\n');
   }
 
   verse(number) {