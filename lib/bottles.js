sandimetz/99bottles_js_milk

Send quantity from default case, phrase 2

lib/bottles.js
@@ -27,7 +27,9 @@ class Bottles {
           `${capitalize(this.quantity(number))} ` +
             `${this.container(number)} ` +
             'of milk on the wall, ' +
-          `${number} ${this.container(number)} of milk.\n` +
+          `${this.quantity(number)} ` +
+            `${this.container(number)} ` +
+            'of milk.\n' +
           `Take ${this.pronoun(number)} down and pass it around, ` +
           `${this.quantity(number-1)} ` +
             `${this.container(number-1)} ` +

sandimetz/99bottles_js

lib/bottles.js
@@ -27,7 +27,9 @@ class Bottles {
           `${capitalize(this.quantity(number))} ` +
             `${this.container(number)} ` +
             'of beer on the wall, ' +
-          `${number} ${this.container(number)} of beer.\n` +
+          `${this.quantity(number)} ` +
+            `${this.container(number)} ` +
+            'of beer.\n' +
           `Take ${this.pronoun(number)} down and pass it around, ` +
           `${this.quantity(number-1)} ` +
             `${this.container(number-1)} ` +