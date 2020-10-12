sandimetz/99bottles_js_milk

Interpolate number into verse string

lib/bottles.js
@@ -1,18 +1,10 @@
 class Bottles {
   verse(number) {
-    let n;
-
-    if (number === 99) {
-      n = 99;
-    } else {
-      n = 3;
-    }
-
     return (
-      `${n} bottles of milk on the wall, ` +
-      `${n} bottles of milk.\n` +
+      `${number} bottles of milk on the wall, ` +
+      `${number} bottles of milk.\n` +
       'Take one down and pass it around, ' +
-      `${n - 1} bottles of milk on the wall.\n`
+      `${number-1} bottles of milk on the wall.\n`
     );
   }
 }

sandimetz/99bottles_js

lib/bottles.js
@@ -1,18 +1,10 @@
 class Bottles {
   verse(number) {
-    let n;
-
-    if (number === 99) {
-      n = 99;
-    } else {
-      n = 3;
-    }
-
     return (
-      `${n} bottles of beer on the wall, ` +
-      `${n} bottles of beer.\n` +
+      `${number} bottles of beer on the wall, ` +
+      `${number} bottles of beer.\n` +
       'Take one down and pass it around, ' +
-      `${n - 1} bottles of beer on the wall.\n`
+      `${number-1} bottles of beer on the wall.\n`
     );
   }
 }