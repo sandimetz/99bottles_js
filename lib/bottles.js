sandimetz/99bottles_js_milk

Implement six-pack as conditional

lib/bottles.js
@@ -34,6 +34,20 @@ class Bottles {
           'Take one down and pass it around, ' +
           '1 bottle of milk on the wall.\n'
         );
+      case 6:
+        return (
+          '1 six-pack of milk on the wall, ' +
+          '1 six-pack of milk.\n' +
+          'Take one down and pass it around, ' +
+          '5 bottles of milk on the wall.\n'
+        );
+      case 7:
+        return (
+          '7 bottles of milk on the wall, ' +
+          '7 bottles of milk.\n' +
+          'Take one down and pass it around, ' +
+          '1 six-pack of milk on the wall.\n'
+        );
       default:
         return (
           `${number} bottles of milk on the wall, ` +

sandimetz/99bottles_js

lib/bottles.js
@@ -34,6 +34,20 @@ class Bottles {
           'Take one down and pass it around, ' +
           '1 bottle of beer on the wall.\n'
         );
+      case 6:
+        return (
+          '1 six-pack of beer on the wall, ' +
+          '1 six-pack of beer.\n' +
+          'Take one down and pass it around, ' +
+          '5 bottles of beer on the wall.\n'
+        );
+      case 7:
+        return (
+          '7 bottles of beer on the wall, ' +
+          '7 bottles of beer.\n' +
+          'Take one down and pass it around, ' +
+          '1 six-pack of beer on the wall.\n'
+        );
       default:
         return (
           `${number} bottles of beer on the wall, ` +