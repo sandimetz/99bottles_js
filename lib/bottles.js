sandimetz/99bottles_js_milk

Make verses pass by looping

lib/bottles.js
@@ -1,11 +1,10 @@
+import { downTo } from './helpers';
+
 class Bottles {
   verses(starting, ending) {
-    if (starting === 99) {
-      return this.verse(99) + '\n' + this.verse(98);
-    } else {
-      return this.verse(2) + '\n' + this.verse(1) +
-        '\n' + this.verse(0);
-    }
+    return downTo(starting, ending)
+      .map(i => this.verse(i))
+      .join('\n');
   }
 
   verse(number) {

sandimetz/99bottles_js

lib/bottles.js
@@ -1,11 +1,10 @@
+import { downTo } from './helpers';
+
 class Bottles {
   verses(upper, lower) {
-    if (upper === 99) {
-      return this.verse(99) + '\n' + this.verse(98);
-    } else {
-      return this.verse(2) + '\n' + this.verse(1) +
-        '\n' + this.verse(0);
-    }
+    return downTo(upper, lower)
+      .map(i => this.verse(i))
+      .join('\n');
   }
 
   verse(number) {