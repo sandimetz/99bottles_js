sandimetz/99bottles_js_milk

Copy verse from Bottles to BottleVerse

lib/bottles.js
@@ -27,6 +27,17 @@ class BottleVerse {
   constructor(number) {
     this.number = number;
   }
+
+  verse(number) {
+    const bottleNumber = BottleNumber.for(number);
+
+    return (
+      capitalize(`${bottleNumber} of milk on the wall, `) +
+      `${bottleNumber} of milk.\n` +
+      `${bottleNumber.action()}, ` +
+      `${bottleNumber.successor()} of milk on the wall.\n`
+    );
+  }
 }
 
 class BottleNumber {

sandimetz/99bottles_js

lib/bottles.js
@@ -27,6 +27,17 @@ class BottleVerse {
   constructor(number) {
     this.number = number;
   }
+
+  verse(number) {
+    const bottleNumber = BottleNumber.for(number);
+
+    return (
+      capitalize(`${bottleNumber} of beer on the wall, `) +
+      `${bottleNumber} of beer.\n` +
+      `${bottleNumber.action()}, ` +
+      `${bottleNumber.successor()} of beer on the wall.\n`
+    );
+  }
 }
 
 class BottleNumber {