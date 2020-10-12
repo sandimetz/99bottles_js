sandimetz/99bottles_js_milk

Send quantity message from 1 case

lib/bottles.js
@@ -26,7 +26,7 @@ class Bottles {
             'of milk on the wall, ' +
           `${number} ${this.container(number)} of milk.\n` +
           `Take ${this.pronoun(number)} down and pass it around, ` +
-          'no more bottles of milk on the wall.\n'
+          `${this.quantity()} bottles of milk on the wall.\n`
         );
       default:
         return (

sandimetz/99bottles_js

lib/bottles.js
@@ -26,7 +26,7 @@ class Bottles {
             'of beer on the wall, ' +
           `${number} ${this.container(number)} of beer.\n` +
           `Take ${this.pronoun(number)} down and pass it around, ` +
-          'no more bottles of beer on the wall.\n'
+          `${this.quantity()} bottles of beer on the wall.\n`
         );
       default:
         return (