sandimetz/99bottles_js_milk

Use bottleNumber property in lyrics

lib/bottles.js
@@ -34,10 +34,10 @@ class BottleVerse {
     const bottleNumber = this.number;
 
     return (
-      capitalize(`${bottleNumber} of milk on the wall, `) +
-      `${bottleNumber} of milk.\n` +
-      `${bottleNumber.action()}, ` +
-      `${bottleNumber.successor()} of milk on the wall.\n`
+      capitalize(`${this.bottleNumber} of milk on the wall, `) +
+      `${this.bottleNumber} of milk.\n` +
+      `${this.bottleNumber.action()}, ` +
+      `${this.bottleNumber.successor()} of milk on the wall.\n`
     );
   }
 }

sandimetz/99bottles_js

lib/bottles.js
@@ -34,10 +34,10 @@ class BottleVerse {
     const bottleNumber = this.number;
 
     return (
-      capitalize(`${bottleNumber} of beer on the wall, `) +
-      `${bottleNumber} of beer.\n` +
-      `${bottleNumber.action()}, ` +
-      `${bottleNumber.successor()} of beer on the wall.\n`
+      capitalize(`${this.bottleNumber} of beer on the wall, `) +
+      `${this.bottleNumber} of beer.\n` +
+      `${this.bottleNumber.action()}, ` +
+      `${this.bottleNumber.successor()} of beer on the wall.\n`
     );
   }
 }