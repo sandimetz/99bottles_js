sandimetz/99bottles_js_milk

Add verse 7 test

test/bottles.test.js
@@ -359,6 +359,15 @@ describe('BottleVerse', () => {
     expect(BottleVerse.lyrics(3)).toBe(expected);
   });
 
+  test('verse 7', () => {
+    const expected =
+      '7 bottles of milk on the wall, ' +
+      '7 bottles of milk.\n' +
+      'Take one down and pass it around, ' +
+      '1 six-pack of milk on the wall.\n';
+    expect(BottleVerse.lyrics(7)).toBe(expected);
+  });
+
   test('verse 2', () => {
     const expected =
       '2 bottles of milk on the wall, ' +

sandimetz/99bottles_js

test/bottles.test.js
@@ -359,6 +359,15 @@ describe('BottleVerse', () => {
     expect(BottleVerse.lyrics(3)).toBe(expected);
   });
 
+  test('verse 7', () => {
+    const expected =
+      '7 bottles of beer on the wall, ' +
+      '7 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '1 six-pack of beer on the wall.\n';
+    expect(BottleVerse.lyrics(7)).toBe(expected);
+  });
+
   test('verse 2', () => {
     const expected =
       '2 bottles of beer on the wall, ' +